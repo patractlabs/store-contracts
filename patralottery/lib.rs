8b0dc443f988d1e964aeb8d20499f664c6257291
zhangtailin <tailinzhang1993@gmail.com>
2021-01-28 00:48:15 +0800

    replace custom env

--- patralottery/lib.rs
+++ patralottery/lib.rs
@@ -1,50 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_env::Environment;
 use ink_lang as ink;
-pub enum CustomEnvironment {}
-
-#[derive(Debug, PartialEq, scale::Decode)]
-pub struct Randomness {
-    pub epoch: u64,
-    pub randomness: <ink_env::DefaultEnvironment as Environment>::Hash,
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
-pub enum ErrorCode {
-    InvalidKey,
-}
-
-impl ink_env::chain_extension::FromStatusCode for ErrorCode {
-    fn from_status_code(status_code: u32) -> Result<(), Self> {
-        match status_code {
-            0 => Ok(()),
-            1 => Err(Self::InvalidKey),
-            _ => panic!("encountered unknown status code"),
-        }
-    }
-}
-
-#[ink::chain_extension]
-pub trait BabeRandomness {
-    type ErrorCode = ErrorCode;
-
-    /// Reads from runtime storage.
-    #[ink(extension = 0x02000000, returns_result = false)]
-    fn random(subject: &[u8]) -> Randomness;
-}
-
-impl Environment for CustomEnvironment {
-    const MAX_EVENT_TOPICS: usize = <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
-
-    type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
-    type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
-    type Hash = <ink_env::DefaultEnvironment as Environment>::Hash;
-    type BlockNumber = <ink_env::DefaultEnvironment as Environment>::BlockNumber;
-    type Timestamp = <ink_env::DefaultEnvironment as Environment>::Timestamp;
-
-    type ChainExtension = BabeRandomness;
-}
+use ink_babe_random::{CustomEnvironment, Randomness};
 
 #[ink::contract(env = crate::CustomEnvironment)]
 mod patralottery {
