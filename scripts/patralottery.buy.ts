091c4fd52f2b2aef1530f49b78acee965d8272b2
zhangtailin <tailinzhang1993@gmail.com>
2021-03-01 14:31:03 +0800

    refine lottery scripts

--- scripts/patralottery.buy.ts
+++ scripts/patralottery.buy.ts
@@ -7,7 +7,9 @@ const { createSigner, keyring, api } = network;
 const uri =
   'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice';
 
-const lotteryContract = '5H9qG8u7dFBTVWontBakVGL2yoqwfDdG2BCpwcBgViQXz9J7';
+const lotteryContract = '5FcjY9tStk6LjTsFCUDZXFPWugrkVaonwcdppVh6xi58QD4j';
+
+const ticketsNum = 10;
 
 async function run() {
   await api.isReady;
@@ -17,19 +19,31 @@ async function run() {
   console.log('Balance: ', balance.toHuman());
 
   const one = new BN(10).pow(new BN(api.registry.chainDecimals));
-  const sender = await getRandomSigner(signer, one.muln(12));
+  const sender = await getRandomSigner(signer, one.muln(100));
   const contract = await getContractAt('patralottery', lotteryContract, sender);
   const epoch = await contract.query.latestEpoch();
   const epochId = epoch.output?.epoch_id.toHuman();
   console.log('Latest epoch: ', epochId);
-  const num1 = Math.floor(Math.random() * 10);
-  const num2 = Math.floor(Math.random() * 10);
-  const num3 = Math.floor(Math.random() * 10);
-  const amount = Math.floor(Math.random() * 10 + 1);
-  console.log('Buy tickets: ', num1, num2, num3, "amount: ", amount);
-  await contract.tx.buyTickets(epochId, [num1, num2, num3], amount, {
-    value: 10000000000 * amount
-  })
+  // let reqArr = [];
+  for (let i=0; i<ticketsNum; i++) {
+    const num1 = Math.floor(Math.random() * 10);
+    const num2 = Math.floor(Math.random() * 10);
+    const num3 = Math.floor(Math.random() * 10);
+    const amount = Math.floor(Math.random() * 5 + 1);
+    // const buyer = await getRandomSigner(signer, one.muln(amount+1));
+    console.log('Buy tickets: ', num1, num2, num3, "amount: ", amount);
+    // let req = contract.tx.buyTickets(epochId, [num1, num2, num3], amount, {
+    //   signer: buyer,
+    //   value: 10000000000 * amount
+    // });
+    // reqArr.push(req);
+    await contract.tx.buyTickets(epochId, [num1, num2, num3], amount, {
+      value: 10000000000 * amount
+    })
+  }
+  // await Promise.all(reqArr).then((values) => {
+  //   console.log(values);
+  // })
 
   api.disconnect();
 }
