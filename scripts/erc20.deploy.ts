b5807f4b167433a710d6bdc4340a7897a4e20a58
tyler <tailinzhang1993@gmail.com>
2021-07-30 13:32:15 +0800

    refine erc20 fixed scripts

--- scripts/erc20.deploy.ts
+++ scripts/erc20.deploy.ts
@@ -1,36 +1,34 @@
-import { patract, network } from 'redspot';
+import {patract, network} from 'redspot';
 
-const { getContractFactory } = patract;
-const { createSigner, keyring, api } = network;
-
-const uri =
-  'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice';
+const {getContractFactory} = patract;
+const {api, getSigners} = network;
 
 async function run() {
-  await api.isReady;
+    await api.isReady;
 
-  const signer = createSigner(keyring.createFromUri(uri));
-  const contractFactory = await getContractFactory('erc20_fixed', signer);
+    const signers = await getSigners();
+    const signer = signers[0];
+    const contractFactory = await getContractFactory('erc20_fixed', signer);
 
-  const balance = await api.query.system.account(signer.address);
+    const balance = await api.query.system.account(signer.address);
 
-  console.log('Balance: ', balance.toHuman());
+    console.log('Balance: ', balance.toHuman());
 
-  const contract = await contractFactory.deployed('new', '10000000000000000', 'Jupiter Token', 'JPT', '10', {
-    gasLimit: '200000000000',
-    value: '0',
-    salt: 'Jupiter Token'
-  });
+    const contract = await contractFactory.deployed('new', '10000000000000000', 'Jupiter Token', 'JPT', '10', {
+        gasLimit: '200000000000',
+        value: '0',
+        salt: 'Jupiter Token'
+    });
 
-  console.log('');
-  console.log(
-    'Deploy successfully. The contract address: ',
-    contract.address.toString()
-  );
+    console.log('');
+    console.log(
+        'Deploy successfully. The contract address: ',
+        contract.address.toString()
+    );
 
-  api.disconnect();
+    api.disconnect();
 }
 
 run().catch((err) => {
-  console.log(err);
+    console.log(err);
 });
